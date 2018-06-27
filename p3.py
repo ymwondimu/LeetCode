67701d41a8cef3a544c004dcf6c931976c7d8548
Yeab Wondimu <ywondimu6@gatech.edu>
2018-06-27 12:53:32 -0700

    Added another function using different method - improved efficiency, solves problem.

--- p3.py
+++ p3.py
@@ -25,8 +25,39 @@ def lengthOfLongestSubstring(s):
 
     return max1
 
+
+def efficient_length(s):
+    current_length = 1
+    max_length = 1
+
+    ht = {}
+    ht[s[0]] = 0
+
+    for i in range(1, len(s)):
+
+        if s[i] not in ht.keys() or (i - ht[s[i]] > current_length):
+            current_length+=1
+
+        else:
+
+            if max_length < current_length:
+                max_length = current_length
+
+            current_length = i - ht[s[i]]
+
+        ht[s[i]] = i
+
+    if max_length < current_length:
+        max_length = current_length
+
+    return max_length
+
+
+
+
 def main():
-    max = lengthOfLongestSubstring("abca")
+    #max = lengthOfLongestSubstring("abca")
+    max = efficient_length("abca")
     print (max)
 
 if __name__ == "__main__":
