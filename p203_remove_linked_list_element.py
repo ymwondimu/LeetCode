cab55098bfb09901ae807087255e3b1d639001b2
Yeab Wondimu <ywondimu6@gatech.edu>
2018-08-19 21:52:12 -0700

    Completed LC 203

---
+++ p203_remove_linked_list_element.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+
+        if head == None:
+            return
+
+        if head.next == None:
+            if head.val == val:
+                return
+
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        curr = dummy
+
+        while (curr.next):
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+
+        return dummy.next
