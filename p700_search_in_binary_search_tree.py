3a768073371c522cff0b0731590653ae561a1d91
Yeab Wondimu <ywondimu6@gatech.edu>
2018-09-03 01:37:11 -0700

    LC #138

---
+++ p700_search_in_binary_search_tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+
+        while (root):
+            if root.val == val:
+                return root
+            elif val < root.val:
+                root = root.left
+            else:
+                root = root.right
+        return []
