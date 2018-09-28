e68dc9688c94153791fe7ff81227a46281f79257
Yeab Wondimu <ywondimu6@gatech.edu>
2018-09-28 07:34:23 -0700

    LC #278

---
+++ p278_first_bad_version.py
@@ -0,0 +1,23 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        left = 0
+        right = n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
